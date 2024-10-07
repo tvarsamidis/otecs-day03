a625057cf1690c444f25862f9b3b8a6f7067f974
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-10-07 14:19:06 +0300

    added sayStatus

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -1,16 +1,28 @@
 public class MyFirstApp {
+
+    public static void sayStatus(String name){
+        if (name.length() == 0){
+            name = "nobody";
+        }
+        System.out.println("Check done by " + name);
+    }
+
+
     public static void main(String[] args) {
         int num1 = 200;
+        sayStatus("");
         int num2 = 800000000; // thomas
         int num3 = 100;
         int max = num1;
         if (max < num2) {
             max = num2;
         }
+        sayStatus("Giota");
         if (max < num3) {
             max = num3;
         }
         System.out.println("The largest number of " + num1 + ", " + num2 + ", " + num3 +" is " + max);
+        sayStatus("Dimitris");
 
     }
 }
