108f854457b9d00d817eb475950a3148a68b7d6c
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-10-07 13:55:35 +0300

    updated 400 to 401

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -1,7 +1,7 @@
 public class MyFirstApp {
     public static void main(String[] args) {
         int num1 = 200;
-        int num2 = 400; // thomas
+        int num2 = 401; // thomas
         int num3 = 100;
         int max = num1;
         if (max < num2) {
