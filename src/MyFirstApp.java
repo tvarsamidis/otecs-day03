df4de6ea17cfe34f548d771fbbeaecd33a39b7b2
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-10-07 15:18:11 +0300

    added maxOf3

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -7,21 +7,45 @@ public class MyFirstApp {
         System.out.println("Check done by " + name);
     }
 
+    public static void sayStatus(double i) {
+        System.out.println("The double status code is " + i);
+    }
+
+
+    public static void sayStatus(int i) {
+        System.out.println("The int status code is " + i);
+    }
+
+    public static int sayAbsolute(int x) {
+        int absoluteX = x;
+        if (x < 0) {
+            absoluteX = -x;
+        }
+        System.out.println("absolute of " + x + " = " + absoluteX);
+        return absoluteX;
+    }
+
+    public static int maxOf3(int a, int b, int c){
+        int max = a;
+        if (max < b) {
+            max = b;
+        }
+        if (max < c) {
+            max = c;
+        }
+        return max;
+    }
+
 
     public static void main(String[] args) {
         int num1 = 200;
-        sayStatus("");
         int num2 = 800000000; // thomas
         int num3 = 100;
-        int max = num1;
-        if (max < num2) {
-            max = num2;
-        }
-        sayStatus("Giota");
-        if (max < num3) {
-            max = num3;
-        }
-        System.out.println("The largest number of " + num1 + ", " + num2 + ", " + num3 +" is " + max);
+
+        int max = maxOf3(num1, num2, num3);
+
+        System.out.println("The largest number of " + num1 +
+                           ", " + num2 + ", " + num3 +" is " + max);
         sayStatus("Dimitris");
 
     }
